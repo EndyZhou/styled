99a0d9b1e9fe9604b3b9f61adb0d25cada7827ca
ZhouLong <long.zhou@lpstech.com>
2024-12-11 10:11:52 +0800

    first commit

---
+++ styled/index.ts
@@ -0,0 +1,7 @@
+export * from './styled';
+
+export * from './types';
+
+export * from './tools';
+
+export { default as styled } from './styled';
---
+++ styled/props/types.ts
@@ -0,0 +1,5 @@
+import { CustomSpace } from '~/theme/custom';
+
+export type CustomSpacePropValue = CustomSpace | number;
+
+export type CustomLayoutPropValue = CustomSpacePropValue | (string & {}) | number;
